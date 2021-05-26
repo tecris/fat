9b3a5cb619c2f12c35e59d8d878c7a13840adef3
Eugen Cristea <eugen@cristea.org>
2021-05-27 11:09:58 +1200

    showMenu method

--- src/main/java/org/playground/banking/BankAccount.java
+++ src/main/java/org/playground/banking/BankAccount.java
@@ -1,12 +1,20 @@
 package org.playground.banking;
 // https://www.youtube.com/watch?v=wQbEH4tVMJA
 
+import java.util.Scanner;
+
 public class BankAccount {
 
     int balance;
     int previousTransaction;
     String customerName;
     String customerId;
+
+    BankAccount(String cname, String cid)
+    {
+        customerName = cname;
+        customerId = cid;
+    }
 
     void deposit(int amount)
     {
@@ -40,5 +48,26 @@ public class BankAccount {
         {
             System.out.println("No transaction occurred");
         }
+    }
+
+    void showMenu()
+    {
+        char option = '\0';
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome " + customerName);
+        System.out.println("Your ID is " +  customerId);
+        System.out.println("\n");
+        System.out.println("A. Check Balance");
+        System.out.println("B. Deposit");
+        System.out.println("C. Withdraw");
+        System.out.println("D. Previous transaction");
+        System.out.println("E. Exit");
+
+        do
+        {
+
+
+        }while (option != 'E');
+
     }
 }
